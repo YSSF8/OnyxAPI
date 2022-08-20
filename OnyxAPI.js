2f28942f239949a2d1cd2bd22dcf4d77dbcd06d6
YSSF8 <76586183+YSSF8@users.noreply.github.com>
2022-08-20 10:44:19 +0100

    Add files via upload

--- OnyxAPI.js
+++ OnyxAPI.js
@@ -15,6 +15,8 @@ function OnyxAlert(alt) {
     parent.style.left = '35.5%';
     parent.style.borderRadius = '8px';
     parent.style.padding = '5px 22px';
+    parent.style.transform = 'scale(0)';
+    parent.style.transition = 'transform 200ms ease-in-out';
 
     // Title of the panel
     const title = document.createElement('h2');
@@ -28,8 +30,17 @@ function OnyxAlert(alt) {
     btn.classList.add('onyx-lib-btn');
     btn.innerHTML = 'OK';
 
+    // Appearing transition
+    setTimeout(() => {
+        parent.style.transform = 'scale(1)';
+    });
+
+    // Removes the alert after clicking OK
     btn.addEventListener('click', () => {
-        parent.remove();
+        parent.style.transform = 'scale(0)';
+        setTimeout(() => {
+            parent.remove();
+        }, 1000);
     });
 
     // Button style
@@ -71,6 +82,8 @@ function OnyxPrompt(pmt, ipt) {
     parent.style.left = '35.5%';
     parent.style.borderRadius = '8px';
     parent.style.padding = '5px 22px';
+    parent.style.transform = 'scale(0)';
+    parent.style.transition = 'transform 200ms ease-in-out';
 
     // Title of the panel
     const title = document.createElement('h2');
@@ -98,8 +111,17 @@ function OnyxPrompt(pmt, ipt) {
     btn.classList.add('onyx-lib-btn');
     btn.innerHTML = 'OK';
 
+    // Appearing transition
+    setTimeout(() => {
+        parent.style.transform = 'scale(1)';
+    });
+
+    // Removes the prompt after clicking "OK"
     btn.addEventListener('click', () => {
-        parent.remove();
+        parent.style.transform = 'scale(0)';
+        setTimeout(() => {
+            parent.remove();
+        }, 500);
     });
 
     // Button style
@@ -149,13 +171,13 @@ function OnyxToast(text) {
     // Toast style
     toast.style.fontFamily = 'Segoe UI';
     toast.style.background = '#1e1e1e';
-    toast.style.width = 'min-content';
+    toast.style.width = 'max-content';
     toast.style.padding = '8px';
     toast.style.borderRadius = '5px';
     toast.style.cursor = 'default';
     toast.style.position = 'absolute';
     toast.style.bottom = '60px';
-    toast.style.left = '48.2%';
+    toast.style.left = '47%';
     toast.style.opacity = '0';
     toast.style.transition = 'opacity 200ms ease-in-out, bottom 200ms ease-in-out';
 
@@ -173,4 +195,113 @@ function OnyxToast(text) {
             toast.remove();
         }, 1501);
     }, 1500);
+}
+
+// Message from the top
+function OnyxNotice(text) {
+    // Notice creation
+    const nts = document.createElement('div');
+    document.body.appendChild(nts);
+    nts.innerHTML = text;
+    nts.classList.add('onyx-lib-notice');
+
+    // Notice style
+    nts.style.fontFamily = 'Segoe UI';
+    nts.style.background = '#1e1e1e';
+    nts.style.padding = '8px 50px';
+    nts.style.position = 'fixed';
+    nts.style.width = '93.78%';
+    nts.style.left = '0';
+    nts.style.top = '-3rem';
+    nts.style.transition = 'top 200ms ease-in-out';
+
+    // Disappear button creation
+    const da = document.createElement('div');
+    document.querySelector('.onyx-lib-notice').appendChild(da);
+    da.classList.add('onyx-lib-da-notice');
+    da.innerHTML = 'X';
+
+    // Disappear button style
+    da.style.fontFamily = 'Comic Sans MS';
+    da.style.float = 'right';
+    da.style.cursor = 'pointer';
+
+    // Appearing transition
+    setTimeout(() => {
+        nts.style.top = '0';
+    });
+
+    // Click event to the disappear button
+    da.addEventListener('click', () => {
+        // Makes the notice go back to the top
+        nts.style.top = '-3rem';
+
+        // Removes the element
+        setTimeout(() => {
+            nts.remove();
+        }, 1000);
+    });
+}
+
+function OnyxSysNotif(title, ncontent) {
+    // System Notification creation
+    const wn = document.createElement('div');
+    document.body.appendChild(wn);
+    wn.classList.add('onyx-lib-sysnotif');
+
+    // Header creation
+    const header = document.createElement('div');
+    document.querySelector('.onyx-lib-sysnotif').appendChild(header);
+    header.innerHTML = title;
+    header.style.fontSize = '30px';
+    const br = document.createElement('br')
+    document.querySelector('.onyx-lib-sysnotif').appendChild(br);
+
+    // System Notification style
+    wn.style.fontFamily = 'Segoe UI';
+    wn.style.position = 'fixed';
+    wn.style.background = '#1e1e1e';
+    wn.style.padding = '10px';
+    wn.style.borderRadius = '8px';
+    wn.style.bottom = '20px';
+    wn.style.right = '-25rem';
+    wn.style.transition = 'right 200ms ease-in-out';
+
+    // Appearing transition
+    setTimeout(() => {
+        wn.style.right = '20px';
+    });
+
+    // Remove the notification
+    function Remove() {
+        wn.style.right = '-25rem';
+        setTimeout(() => {
+            wn.remove();
+        }, 500);
+    }
+
+    // Removes the notification after 3 seconds by calling the function "Remove()"
+    setTimeout(Remove, 3000);
+
+    // Content creation
+    const content = document.createElement('div');
+    document.querySelector('.onyx-lib-sysnotif').appendChild(content);
+    content.innerHTML = ncontent;
+
+    // Disappear button creation
+    const xbtn = document.createElement('div');
+    document.querySelector('.onyx-lib-sysnotif').appendChild(xbtn);
+    xbtn.innerHTML = 'X';
+
+    // Disappear button style
+    xbtn.style.fontFamily = 'Comic Sans MS';
+    xbtn.style.position = 'absolute';
+    xbtn.style.top = '5px';
+    xbtn.style.right = '11px';
+    xbtn.style.cursor = 'pointer';
+
+    // Removes the notification after clicking on the "X" button by calling the function "Remove()"
+    xbtn.addEventListener('click', () => {
+        Remove();
+    });
 }
